59bed65978d8a40e7e0392669c4e07e8eb9c06b8
julionce <julianbortega@gmail.com>
2023-09-16 10:50:46 +0200

    Take common factor of generate_struct_body_deserialize and generate_table_body_deserialize functions in generate_body_deserialize macros

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -243,64 +243,56 @@ fn generate_type_trait_bounds_deserialize(fields: &syn::Fields) -> Vec<TokenStre
 }
 
 fn generate_body_deserialize(data: &syn::DataStruct, struct_attrs: &StructAttrs) -> TokenStream2 {
-    match struct_attrs.table.0 {
-        true => generate_table_body_deserialize(data, struct_attrs),
-        false => generate_struct_body_deserialize(&data.fields, struct_attrs),
-    }
-}
-
-fn generate_struct_body_deserialize(
-    fields: &syn::Fields,
-    struct_attrs: &StructAttrs,
-) -> TokenStream2 {
     let version_deserialize = generate_version_deserialize(struct_attrs);
-    let field_deserializes = generate_field_deserializes(&fields, struct_attrs);
     match &struct_attrs.from_chunk_version {
         Some(version) => {
             let major_version = version.major;
             let minor_version = version.minor;
-            quote! {
-                #version_deserialize
-                if version.major() >= #major_version && version.minor() >= #minor_version {
-                    Ok(Self {#(#field_deserializes),*})
-                } else {
-                    Ok(Self::default())
+            match struct_attrs.table.0 {
+                true => {
+                    let table_body_loop = generate_table_body_loop(data, struct_attrs);
+                    quote! {
+                        #version_deserialize
+                        if version.major() >= #major_version && version.minor() >= #minor_version {
+                            let mut table = Self::default();
+                            #table_body_loop
+                            Ok(table)
+                        } else {
+                            Ok(Self::default())
+                        }
+                    }
+                }
+                false => {
+                    let field_deserializes =
+                        generate_field_deserializes(&data.fields, struct_attrs);
+                    quote! {
+                        #version_deserialize
+                        if version.major() >= #major_version && version.minor() >= #minor_version {
+                            Ok(Self {#(#field_deserializes),*})
+                        } else {
+                            Ok(Self::default())
+                        }
+                    }
                 }
             }
         }
-        None => quote! {
-            #version_deserialize
-            Ok(Self {#(#field_deserializes),*})
-        },
-    }
-}
-
-fn generate_table_body_deserialize(
-    data: &syn::DataStruct,
-    struct_attrs: &StructAttrs,
-) -> TokenStream2 {
-    let version_deserialize = generate_version_deserialize(struct_attrs);
-    let table_body_loop = generate_table_body_loop(data, struct_attrs);
-    match &struct_attrs.from_chunk_version {
-        Some(version) => {
-            let major_version = version.major;
-            let minor_version = version.minor;
-            quote! {
-                #version_deserialize
-                if version.major() >= #major_version && version.minor() >= #minor_version {
+        None => match struct_attrs.table.0 {
+            true => {
+                let table_body_loop = generate_table_body_loop(data, struct_attrs);
+                quote! {
+                    #version_deserialize
                     let mut table = Self::default();
                     #table_body_loop
                     Ok(table)
-                } else {
-                    Ok(Self::default())
                 }
             }
-        }
-        None => quote! {
-            let mut table = Self::default();
-            #version_deserialize
-            #table_body_loop
-            Ok(table)
+            false => {
+                let field_deserializes = generate_field_deserializes(&data.fields, struct_attrs);
+                quote! {
+                    #version_deserialize
+                    Ok(Self {#(#field_deserializes),*})
+                }
+            }
         },
     }
 }
