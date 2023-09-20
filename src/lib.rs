5bec8cf00f36133761574aa073f41e653f4042b2
julionce <julianbortega@gmail.com>
2023-09-20 20:33:20 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,16 @@
+#![feature(seek_stream_len)]
+#![feature(int_roundings)]
+
+pub mod application;
+pub mod bitmap;
 pub mod chunk;
 pub mod comment;
+pub mod compressed_buffer;
 pub mod deserialize;
 pub mod header;
+pub mod notes;
+pub mod revision_history;
+pub mod sequence;
 pub mod start_section;
 pub mod time;
 pub mod typecode;
