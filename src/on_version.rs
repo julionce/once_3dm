e3a23a1901993f20bda5ad4e9f0b0eec3b3ea525
julionce <julianbortega@gmail.com>
2023-10-04 19:29:47 +0200

    Change OnVersion underlying type from u64 to u32

--- src/on_version.rs
+++ src/on_version.rs
@@ -7,5 +7,5 @@ use crate::deserialize::{Deserialize, FileVersion};
 #[derive(Default, Deserialize)]
 pub struct OnVersion {
     //TODO: process raw version
-    pub raw_version: u64,
+    pub raw_version: u32,
 }
