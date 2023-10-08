99b05897a4da74b2e690873a3bcbbc54c369ff16
julionce <julianbortega@gmail.com>
2023-10-08 18:16:10 +0200

    Add src/error.rs file

---
+++ src/error.rs
@@ -0,0 +1,126 @@
+use std::collections::VecDeque;
+
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum ErrorKind {
+    BufferSizeMismatch,
+    FromUtf16Error,
+    InvalidChunkSize,
+    InvalidChunkTypecode,
+    InvalidCompressionMode,
+    InvalidHeader,
+    InvalidVersion,
+    InvalidStringLength,
+    IoError,
+}
+
+impl ErrorKind {
+    pub fn as_str(&self) -> &'static str {
+        use ErrorKind::*;
+
+        match *self {
+            BufferSizeMismatch => "buffer size mismatch",
+            FromUtf16Error => "from UTF-16 error",
+            InvalidChunkSize => "invalid chunk size",
+            InvalidChunkTypecode => "invalid chunk typecode",
+            InvalidCompressionMode => "invalid compression mode",
+            InvalidHeader => "invalid header",
+            InvalidVersion => "invalid version",
+            InvalidStringLength => "invalid string length",
+            IoError => "IO error",
+        }
+    }
+}
+
+impl std::fmt::Display for ErrorKind {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(self.as_str())
+    }
+}
+
+#[derive(Debug)]
+pub struct ErrorFrame {
+    member: &'static str,
+    ty: &'static str,
+}
+
+impl ErrorFrame {
+    pub fn new(member: &'static str, ty: &'static str) -> Self {
+        Self { member, ty }
+    }
+}
+
+#[derive(Debug, Default)]
+pub struct ErrorTrace {
+    inner: VecDeque<ErrorFrame>,
+}
+
+impl std::fmt::Display for ErrorTrace {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}",
+            self.inner
+                .iter()
+                .map(|frame| format!("\u{21aa} {}: {}", frame.member, frame.ty))
+                .collect::<Vec<String>>()
+                .join("\n")
+        )
+    }
+}
+
+#[derive(Debug)]
+pub enum Error {
+    IoError(std::io::Error),
+    FromUtf16Error(std::string::FromUtf16Error),
+    Simple(ErrorKind),
+}
+
+impl Error {
+    pub fn kind(&self) -> ErrorKind {
+        use Error::*;
+        match self {
+            IoError(_) => ErrorKind::IoError,
+            FromUtf16Error(_) => ErrorKind::FromUtf16Error,
+            Simple(kind) => *kind,
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct ErrorStack {
+    trace: ErrorTrace,
+    origin: Error,
+}
+
+impl ErrorStack {
+    pub fn new(origin: Error) -> Self {
+        Self {
+            trace: ErrorTrace::default(),
+            origin,
+        }
+    }
+
+    pub fn push_frame(&mut self, member: &'static str, ty: &'static str) {
+        self.trace.inner.push_back(ErrorFrame::new(member, ty))
+    }
+
+    pub fn pop_frame(&mut self) -> Option<ErrorFrame> {
+        self.trace.inner.pop_back()
+    }
+
+    pub fn origin(&self) -> &Error {
+        &self.origin
+    }
+}
+
+impl std::fmt::Display for ErrorStack {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match &self.origin {
+            Error::IoError(e) => write!(f, "IoError: {}\n{}", e, self.trace),
+            Error::Simple(kind) => write!(f, "Simple {}\n{}", kind.as_str(), self.trace),
+            Error::FromUtf16Error(e) => write!(f, "{}\n{}", e, self.trace),
+        }
+    }
+}
+
+impl std::error::Error for ErrorStack {}
