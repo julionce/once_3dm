b2e3da6d6e082065d2ce007f33df8ba92df21903
julionce <julianbortega@gmail.com>
2023-10-25 20:56:31 +0200

    Modify Deserialize implementation for Point type

--- src/point.rs
+++ src/point.rs
@@ -1,12 +1,15 @@
 use once_3dm_macros::Deserialize;
 
 use crate::{
+    chunk::{self},
     deserialize,
     deserialize::{Deserialize, FileVersion},
     error::ErrorStack,
 };
 
 #[derive(Default, Deserialize)]
+#[with_version(short)]
+#[if_major_version(Eq(1))]
 pub struct Point {
     pub x: f64,
     pub y: f64,
