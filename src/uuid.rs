05ec46202afabea662f25b24a8f1eef35ce9675e
julionce <julianbortega@gmail.com>
2023-11-05 16:59:24 +0100

    Implement Display trait for Uuid type and fix some object ids

--- src/uuid.rs
+++ src/uuid.rs
@@ -1,3 +1,5 @@
+use std::fmt::{write, Display};
+
 use once_3dm_macros::Deserialize;
 
 use crate::{
@@ -12,4 +14,24 @@ pub struct Uuid {
     pub data2: u16,
     pub data3: u16,
     pub data4: [u8; 8],
+}
+
+impl Display for Uuid {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{:08X}-{:04X}-{:04X}-{:02X}{:02X}-{:02X}{:02X}{:02X}{:02X}{:02X}{:02X}",
+            self.data1,
+            self.data2,
+            self.data3,
+            self.data4[0],
+            self.data4[1],
+            self.data4[2],
+            self.data4[3],
+            self.data4[4],
+            self.data4[5],
+            self.data4[6],
+            self.data4[7],
+        )
+    }
 }
